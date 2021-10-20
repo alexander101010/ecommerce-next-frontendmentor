fa71458de33df1e3b495f272f540da1283686e98
Alexander Botwinick <alexbotwinick@gmail.com>
2021-10-20 17:10:38 +0200

    add link active state fn-ality

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+
+import CartNav from './Cart/CartNav';
+import classes from './MainNavigation.module.scss';
+
 const MainNavigation = () => {
-  return <h1>sneakers</h1>;
+  const [activeLink, setActiveLink] = useState('');
+  const navLinkClickHandler = (e) => {
+    setActiveLink(e.target.textContent);
+  };
+  const navLinks = ['Collections', 'Men', 'Women', 'About', 'Contact'];
+
+  return (
+    <nav className={classes.nav}>
+      <div className={classes['nav-left']}>
+        <h1 className={classes.title}>sneakers</h1>
+        <ul className={classes['nav-list']}>
+          {navLinks.map((link) => (
+            <li
+              onClick={navLinkClickHandler}
+              key={link}
+              className={`${classes['nav-list__item']} ${
+                link === activeLink ? classes.active : ''
+              }`}
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <CartNav />
+    </nav>
+  );
 };
 
 export default MainNavigation;
