938287e8ba0486466f889d0e5ca246ea6de04ef1
Alexander Botwinick <alexbotwinick@gmail.com>
2021-10-21 15:22:07 +0200

    shoeinfo markup and styles

--- components/shoes/ShoeInfo.js
+++ components/shoes/ShoeInfo.js
@@ -1,7 +1,37 @@
+import Button from '../ui/Button';
+import CartSVG from '../svgs/CartSVG';
+
 import classes from './ShoeInfo.module.scss';
+import QuantityStepper from '../ui/QuantityStepper';
 
 const ShoeInfo = () => {
-  return <div className={classes.test}>Test ShoeInfo</div>;
+  const btnClickHandler = (e) => {
+    console.log(e.target.innerText);
+  };
+  return (
+    <div className={classes['info-container']}>
+      <div className={classes.company}>sneaker company</div>
+      <h1 className={classes.headline}>Fall Limited Edition Sneakers</h1>
+      <p className={classes.description}>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they’ll withstand everything the
+        weather can offer.
+      </p>
+      <div className={classes['price-box']}>
+        <p className={classes['sale-price']}>
+          $125.00 <span>50%</span>
+        </p>
+        <p className={classes['list-price']}>$250.00</p>
+      </div>
+      <div className={classes['actions']}>
+        <QuantityStepper />
+        <Button className={'primary'} onClick={btnClickHandler}>
+          <CartSVG className={'btn-cart'} color={'white'} />
+          <span className={'btn-left-margin'}>Add to cart</span>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ShoeInfo;
