2d729515f769802af771220a96f33a161a8b8206
Alexander Botwinick <alexbotwinick@gmail.com>
2021-10-26 16:09:22 +0200

    cleanup - get state from parent comp

--- components/layout/Cart/CartNav.js
+++ components/layout/Cart/CartNav.js
@@ -1,19 +1,17 @@
-import { useState } from 'react';
-
 import CartSVG from '../../svgs/CartSVG';
 
 import classes from './CartNav.module.scss';
 
-const CartNav = () => {
-  const [showCart, setShowCart] = useState(false);
-  const avatarClickHandler = (e) => {
-    setShowCart(true);
-  };
+const CartNav = ({ onClose, onShowCart, showCart }) => {
   return (
     <div className={classes['cart-nav-container']}>
-      <CartSVG color={'hsl(219, 9%, 45%)'} />
+      <CartSVG
+        color={'hsl(219, 9%, 45%)'}
+        onClick={onShowCart}
+        className={'hover-pointer'}
+      />
       <img
-        onClick={avatarClickHandler}
+        onClick={onShowCart}
         className={`${classes.avatar} ${showCart ? classes.active : ''}`}
         src='/images/image-avatar.png'
         alt='Photo of sauve dude'
