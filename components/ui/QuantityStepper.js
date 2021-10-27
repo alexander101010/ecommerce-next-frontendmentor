43057b2705aa3243a1b8eeaf1cc8354cdb03468a
Alexander Botwinick <alexbotwinick@gmail.com>
2021-10-27 15:58:07 +0200

    add class for hover

--- components/ui/QuantityStepper.js
+++ components/ui/QuantityStepper.js
@@ -5,9 +5,9 @@ import PlusSignSVG from '../svgs/PlusSignSVG';
 const QuantityStepper = (props) => {
   return (
     <div className={classes['box']}>
-      <MinusSignSVG className='hover-pointer' />
-      <div className={classes['quantity']}>0</div>
-      <PlusSignSVG className='hover-pointer' />
+      <MinusSignSVG className='hover-pointer' onClick={props.onDecrement} />
+      <div className={classes['quantity']}>{props.quantity}</div>
+      <PlusSignSVG className='hover-pointer' onClick={props.onIncrement} />
     </div>
   );
 };
