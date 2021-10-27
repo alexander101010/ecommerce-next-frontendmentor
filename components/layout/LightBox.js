7cf14e97d73dea9445819ef27f4e59de72b15111
Alexander Botwinick <alexbotwinick@gmail.com>
2021-10-27 15:54:40 +0200

    lightbox markup using modal and shoeGallery components

---
+++ components/layout/LightBox.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import ShoeGallery from '../shoes/ShoeGallery';
+import ExitXSVG from '../svgs/exitXSVG';
+import Modal from '../ui/Modal';
+
+import classes from './LightBox.module.scss';
+
+export default function LightBox(props) {
+  const [xIsHovered, setXisHovered] = useState(false);
+  const [lightBoxImage, setLightBoxImage] = useState(props.selectedImage);
+
+  return (
+    <Modal className='light-box' onClose={props.onClose}>
+      <div
+        onMouseEnter={() => setXisHovered(true)}
+        onMouseLeave={() => setXisHovered(false)}
+        className={classes['exit-box']}
+        onClick={props.onClose}
+      >
+        <ExitXSVG isHovered={xIsHovered} />
+      </div>
+      <ShoeGallery
+        inLightBox={true}
+        setSelectedImage={setLightBoxImage}
+        selectedImage={lightBoxImage}
+      />
+    </Modal>
+  );
+}
