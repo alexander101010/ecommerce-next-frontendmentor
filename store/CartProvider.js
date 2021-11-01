6c8649d322d1beb15cc32b9d391744e36ae7eadd
Alexander Botwinick <alexbotwinick@gmail.com>
2021-11-01 10:01:47 +0100

    use dummy data to preload items into cart context for testing

--- store/CartProvider.js
+++ store/CartProvider.js
@@ -2,20 +2,84 @@ import React, { useReducer } from 'react';
 
 import CartContext from './cart-context';
 
+const dummyCart = [
+  {
+    id: 's1',
+    price: 125,
+    quantity: 3,
+    title: 'Fall Limited Edition Sneakers',
+    imgPath: './images/image-product-1-thumbnail.jpg',
+    imgDescription: 'Sweet Fall Kicks',
+  },
+  {
+    id: 's2',
+    price: 125,
+    quantity: 1,
+    title: 'Fall Limited Edition Sneakers',
+    imgPath: './images/image-product-2-thumbnail.jpg',
+    imgDescription: 'Sweet Fall Kicks',
+  },
+];
+
 const defaultCartState = {
-  items: [],
-  totalAmount: 0,
+  items: dummyCart,
+  // items: [],
 };
 
 const cartReducer = (state, action) => {
-  if (action === 'ADD_ITEMS') {
-    //TODO:
+  if (action.type === 'ADD_ITEMS') {
+    ////////////////////////////////////////
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.payload.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + action.payload.quantity,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.payload);
+    }
+
+    return {
+      items: [...updatedItems],
+    };
   }
-  if (action === 'REMOVE_ITEMS') {
-    //TODO:
+
+  if (action.type === 'REMOVE_ITEMS') {
+    const targetId = action.payload;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === targetId
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    // could add logic to just remove one at a time, but lets just bin all of them
+    if (existingCartItem) {
+      updatedItems = state.items.filter((item) => item.id !== action.payload);
+    }
+
+    return {
+      items: updatedItems,
+    };
+    // console.log(existingCartItemIndex);
+    // const updatedItems = state.items.filter((item) => item.id !== targetId);
+    // console.log(updatedItems);
+    // return {
+    //   items: [...updatedItems],
+    // };
   }
-  if (action === 'CLEAR') {
-    //TODO:
+  if (action.type === 'CLEAR') {
+    ////////////////////////////////////////////
+    return {
+      items: [],
+    };
   }
 
   return defaultCartState;
@@ -24,17 +88,17 @@ const cartReducer = (state, action) => {
 const CartProvider = (props) => {
   const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
 
-  const addItemsToCartHandler = (items) => {
+  const addItemToCartHandler = (item) => {
     dispatchCart({
       type: 'ADD_ITEMS',
-      items: { id: id, quantity: quantity },
+      payload: item,
     });
   };
 
-  const removeItemsFromCartHandler = (items) => {
+  const removeItemsFromCartHandler = (id) => {
     dispatchCart({
       type: 'REMOVE_ITEMS',
-      items: { id: id, quantity: quantity },
+      payload: id,
     });
   };
 
@@ -44,8 +108,7 @@ const CartProvider = (props) => {
 
   const cartContext = {
     items: cartState.items,
-    totalAmount: cartState.totalAmount,
-    addItems: addItemsToCartHandler,
+    addItem: addItemToCartHandler,
     removeItems: removeItemsFromCartHandler,
     clearCart: clearCartHandler,
   };
