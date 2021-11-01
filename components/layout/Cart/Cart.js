fa342e2fa72fa2fed48262d1859d771bfb2736f9
Alexander Botwinick <alexbotwinick@gmail.com>
2021-11-01 09:57:13 +0100

    get rid of dummy data, hook up to cartCtx

--- components/layout/Cart/Cart.js
+++ components/layout/Cart/Cart.js
@@ -1,58 +1,63 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
+
 import Modal from '../../ui/Modal';
 import CartItem from './CartItem';
-
-import classes from './Cart.module.scss';
 import Button from '../../ui/Button';
 
+import classes from './Cart.module.scss';
+
 const Cart = (props) => {
-  const dummyCart = [
-    {
-      shoeId: 's1',
-      shoePrice: 125,
-      shoeQuantity: 3,
-      shoeTitle: 'Fall Limited Edition Sneakers',
-      imgPath: './images/image-product-1-thumbnail.jpg',
-      imgDescription: 'Sweet Fall Kicks',
-    },
-    {
-      shoeId: 's2',
-      shoePrice: 125,
-      shoeQuantity: 2,
-      shoeTitle: 'Fall Limited Edition Sneakers',
-      imgPath: './images/image-product-2-thumbnail.jpg',
-      imgDescription: 'Sweet Fall Kicks',
-    },
-  ];
+  const cartCtx = useContext(CartContext);
 
   const checkoutHandler = (e) => {
     e.preventDefault();
     props.onClose();
     alert('Thank you for your order!');
-    // TODO:clear cart
+    cartCtx.clearCart();
   };
+
+  const deleteItemsHandler = (id) => {
+    cartCtx.removeItems(id);
+  };
+
+  const emptyCartMarkup = (
+    <p className={classes['empty-cart-content']}>Your cart is empty.</p>
+  );
+
+  console.log(cartCtx.items);
+
+  const cartItemsMarkup = (
+    <div className={classes['cart-body']}>
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            imgPath={item.imgPath}
+            imgDescription={item.imgDescription}
+            title={item.title}
+            quantity={item.quantity}
+            price={item.price}
+            onDeleteItem={deleteItemsHandler.bind(null, item.id)}
+          />
+        );
+      })}
+      <Button className='primary full-width' onClick={checkoutHandler}>
+        Checkout
+      </Button>
+    </div>
+  );
+
+  const isCartEmpty = cartCtx.items.length === 0;
+  // console.log(isCartEmpty);
+
   return (
-    <Modal onClose={props.onClose}>
+    <Modal onClose={props.onClose} className='modal-cart'>
       <div className={classes['card-title-box']}>
         <h3 className={classes.title}>Cart</h3>
       </div>
-      <div className={classes['cart-body']}>
-        {dummyCart.map((item, index) => {
-          return (
-            <CartItem
-              key={item.shoeId}
-              id={item.shoeId}
-              imgPath={item.imgPath}
-              imgDescription={item.imgDescription}
-              title={item.shoeTitle}
-              quantity={item.shoeQuantity}
-              price={item.shoePrice}
-            />
-          );
-        })}
-        <Button className='primary full-width' onClick={checkoutHandler}>
-          Checkout
-        </Button>
-      </div>
+      {isCartEmpty ? emptyCartMarkup : cartItemsMarkup}
     </Modal>
   );
 };
